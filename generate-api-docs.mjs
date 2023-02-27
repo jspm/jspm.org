f62e78976b0cb2b9f55b8e6f76d9f1a46a8d6d71
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2023-02-27 12:41:29 +0200

    WIP

--- generate-api-docs.mjs
+++ generate-api-docs.mjs
@@ -1,97 +1,81 @@
 import { spawnSync } from 'child_process';
 import { createInterface } from 'readline';
 import * as path from "path";
+import * as fs from "fs";
+import pc from "picocolors";
+
+const log = (msg) => console.log(pc.bold(msg));
 
 const submodules = [
-  "generator", // @jspm/generator
+  // "generator", // @jspm/generator
   "import-map", // @jspm/import-map
 ];
 
 for (const submodule of submodules) {
-  console.log(`Generating API documentation for: ${submodule}`);
+  log(`Generating API documentation for: ${submodule}`);
   const currentDir = path.dirname(new URL(import.meta.url).pathname);
   const submodulePath = path.resolve(currentDir, `api_repos/${submodule}`);
+  const submoduleDocsPath = path.resolve(submodulePath, "docs");
+  const outputDocsPath = path.resolve(currentDir, "public_html/api", submodule);
   const typedocJsonPath = path.resolve(submodulePath, "typedoc.json");
   const tsJsonPath = path.resolve(submodulePath, "tsconfig.json");
 
-  // First run "npm install" in the submodule:
-  console.log("   Installing dependencies...");
-  const installResult = spawnSync("npm", ["install"], {
+  // Fetch the current git branch:
+  const gitBranch = spawnSync("git", ["rev-parse", "--abbrev-ref", "HEAD"], {
     cwd: submodulePath,
-    stdio: "inherit",
-  });
-  if (installResult.status !== 0) {
-    console.error(installResult.error);
-    process.exit(1);
-  }
+    encoding: "utf8",
+  }).stdout.trim();
+  log(`Current git branch: ${gitBranch}`);
 
-  // Then we run typedoc to generate the API docs:
-  console.log("   Generating API documentation...");
-  const typedocResult = spawnSync("npx", [
-    "typedoc",
-    "--tsconfig", tsJsonPath,
-    "--options", typedocJsonPath,
-  ], {
+  // For retro-generating old tags, we need to inject the typedoc and
+  // typescript config files into the submodule after every checkout:
+  const typedocJson = fs.readFileSync(typedocJsonPath, "utf8");
+  const tsJson = fs.readFileSync(tsJsonPath, "utf8");
+
+  // We generate API docs for every published semver tag in the submodule:
+  const tags = spawnSync("git", ["tag", "--list"], {
     cwd: submodulePath,
-    stdio: "inherit",
-  });
-  if (typedocResult.status !== 0) {
-    console.error(typedocResult.error);
-    process.exit(1);
+    encoding: "utf8",
+  }).stdout.split("\n")
+    .map((tag) => tag.trim())
+    .filter((tag) => tag.match(/^\d+\.\d+\.\d+$/));
+  log("   Found version tags: ", tags);
+
+  for (const tag of tags) {
+    log(`   Generating docs for version: ${tag}`);
+    spawnSync("git", ["checkout", tag], { cwd: submodulePath });
+
+    // We need to inject various configs as older tags don't have the rigging
+    // we added for this script to work. We also need to copy over the current
+    // build for idempotency:
+    fs.writeFileSync(typedocJsonPath, typedocJson);
+    fs.writeFileSync(tsJsonPath, tsJson);
+    const initialCopyRes = spawnSync("cp", ["-r", outputDocsPath, submoduleDocsPath]);
+    if (initialCopyRes.status !== 0) {
+      console.error(initialCopyRes.error);
+      process.exit(1);
+    }
+
+    spawnSync("npx", [
+      "typedoc",
+      "--tsconfig", tsJsonPath,
+      "--options", typedocJsonPath,
+    ], { cwd: submodulePath });
+
+    // Copy results back to current build:
+    //const copyRes = spawnSync("cp", ["-r", submoduleDocsPath, outputDocsPath]);
+    //if (copyRes.status !== 0) {
+    //  console.error(copyRes.error);
+    //  process.exit(1);
+    //}
+
+    // Clean out everything in the submodule for next version generation:
+    //spawnSync("git", ["clean", "-fdx"], { cwd: submodulePath });
+    //spawnSync("git", ["checkout", "."], { cwd: submodulePath });
   }
 
-  // Purge existing documentation:
-  await new Promise((resolve) => {
-    const readline = createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-    readline.question(`   Purge existing documentation for ${submodule}? (y/N) `, (answer) => {
-      readline.close();
-      if (answer.toLowerCase() === 'y') {
-        console.log("   Purging existing documentation...");
-        const purgeResult = spawnSync("rm", [
-          "-rf",
-          path.resolve(currentDir, "public_html/api", submodule),
-        ], {
-          stdio: "inherit",
-        });
-        if (purgeResult.status !== 0) {
-          console.error(purgeResult.error);
-          process.exit(1);
-        }
-      }
-      resolve();
-    });
-  });
-
-  // Next, we copy the generated documentation to the public_html/api folder:
-  console.log("   Copying generated documentation...");
-  const copyResult = spawnSync("cp", [
-    "-r",
-    path.resolve(submodulePath, "docs"),
-    path.resolve(currentDir, "public_html/api", submodule),
-  ], {
-    stdio: "inherit",
-  });
-  if (copyResult.status !== 0) {
-    console.error(copyResult.error);
-    process.exit(1);
-  }
-
-  // Finally, reset the submodule hard:
-  console.log("   Resetting submodule...");
-  const resetResult = spawnSync("rm", [
-    "-r",
-    "docs",
-  ], {
-    cwd: submodulePath,
-    stdio: "inherit",
-  });
-  if (resetResult.status !== 0) {
-    console.error(resetResult.error);
-    process.exit(1);
-  }
+  // Reset the submodule to the original branch:
+  spawnSync("git", ["checkout", gitBranch], { cwd: submodulePath });
 }
 
-console.log("Done!");
+log("Done!");
