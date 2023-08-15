246c1f7832c791cfcf029394bba1bc2c6a0a2c98
Jaya Krishna Namburu <namburu1995@gmail.com>
2023-08-15 22:36:46 +0530

    Update documentation for all latest packages (#37)

---
+++ public_html/docs/jspm-cli/v3.0.2/assets/search.js
@@ -0,0 +1,1 @@
+window.searchData = JSON.parse("{\"rows\":[{\"kind\":32,\"name\":\"cli\",\"url\":\"variables/cli.html\",\"classes\":\"\"}],\"index\":{\"version\":\"2.3.9\",\"fields\":[\"name\",\"comment\"],\"fieldVectors\":[[\"name/0\",[0,2.877]],[\"comment/0\",[]]],\"invertedIndex\":[[\"cli\",{\"_index\":0,\"name\":{\"0\":{}},\"comment\":{}}]],\"pipeline\":[]}}");
