9984b2a52c0d5c9a10a9d422397b14879d5b11ad
JayaKrishnaNamburu <namburu1995@gmail.com>
2023-08-15 23:27:37 +0530

    Use `npx` for using local version of typeedoc in chomp extensions

--- extensions/typedoc.js
+++ extensions/typedoc.js
@@ -1,44 +1,50 @@
-Chomp.registerTemplate('typedoc-generator', function (task) {
-  let {
-    lib,
-    out,
-    plugins = '',
-    flags = ''
-  } = task.templateOptions;
+Chomp.registerTemplate("typedoc-generator", function (task) {
+  let { lib, out, plugins = "", flags = "" } = task.templateOptions;
 
-  plugins = plugins.split(',');
+  plugins = plugins.split(",");
 
-  const envVersion = `${lib.toUpperCase().replace(/-/g, '_')}_VERSION`;
+  const envVersion = `${lib.toUpperCase().replace(/-/g, "_")}_VERSION`;
 
-  if (!lib)
-    throw new Error(`Must provide a "lib" path to build with TypeDoc`);
+  if (!lib) throw new Error(`Must provide a "lib" path to build with TypeDoc`);
   if (!out)
     throw new Error(`Must provide an "out" path to build documentation into`);
   if (!ENV[envVersion])
-    throw new Error(`Must provide a ${envVersion} env var to the typedoc generator, matching the package.json version of the project being built`);
+    throw new Error(
+      `Must provide a ${envVersion} env var to the typedoc generator, matching the package.json version of the project being built`
+    );
 
-  const libLen = lib.split('/').length;
-  const backtrack = '../'.repeat(libLen);
+  const libLen = lib.split("/").length;
+  const backtrack = "../".repeat(libLen);
 
-  return [{
-    name: task.name,
-    target: `${out}/v${ENV[envVersion]}`,
-    deps: [
-      `${lib}/src/**/*.ts`,
-      `${lib}/package.json`,
-      `${lib}/typedoc.json`,
-      `${lib}/tsconfig.json`,
-      ...plugins.filter(plugin => plugin.startsWith('./')).map(plugin => plugin.slice(2)),
-      ...task.deps || []
-    ],
-    cwd: lib,
-    run: `
-      typedoc --searchInComments --categorizeByGroup false --skipErrorChecking \
+  return [
+    {
+      name: task.name,
+      target: `${out}/v${ENV[envVersion]}`,
+      deps: [
+        `${lib}/src/**/*.ts`,
+        `${lib}/package.json`,
+        `${lib}/typedoc.json`,
+        `${lib}/tsconfig.json`,
+        ...plugins
+          .filter((plugin) => plugin.startsWith("./"))
+          .map((plugin) => plugin.slice(2)),
+        ...(task.deps || []),
+      ],
+      cwd: lib,
+      run: `
+      npx typedoc --searchInComments --categorizeByGroup false --skipErrorChecking \
         --tsconfig tsconfig.json \
         --options typedoc.json \
-        ${plugins.map(plugin => plugin.startsWith('./') ? `--plugin ${backtrack}${plugin.slice(2)}` : `--plugin ${plugin}`).join(' ')} \
+        ${plugins
+          .map((plugin) =>
+            plugin.startsWith("./")
+              ? `--plugin ${backtrack}${plugin.slice(2)}`
+              : `--plugin ${plugin}`
+          )
+          .join(" ")} \
         ${flags} \
         --out ${backtrack}${out}
-    `
-  }];
+    `,
+    },
+  ];
 });
