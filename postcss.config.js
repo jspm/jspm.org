56d2e737327a3b0d57948343794dc0f69394fb5c
Jeff Wainwright <yowainwright@gmail.com>
2024-05-04 00:23:37 -0700

    chore: adds working tailwind setup

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+import cssnano from 'cssnano';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [
+    cssnano,
+    tailwindcss,
+    autoprefixer
+  ]
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["template.html", "./public_html/*.html"],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      themeTint: '#C2BEA9',
+      themeTitle: '#F9E566',
+      themeBackground: '#FEF9DD',
+    },
     extend: {},
   },
   plugins: [],
 }
-
