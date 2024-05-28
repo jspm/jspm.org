16a9a50129907591624abb50810f8d25e9d8b67e
Jeff Wainwright <yowainwright@gmail.com>
2024-05-28 03:14:44 -0700

    chore: updates with work reload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import daisyui from "daisyui"
+
 export default {
   content: ["template.html", "./public_html/*.html"],
   theme: {
@@ -10,5 +13,35 @@ export default {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    daisyui,
+  ],
+  daisyui: {
+    themes: [
+      {
+        jspmLight: {
+          primary: '#000',
+          secondary: '#374151',
+          accent: '#facc15',
+          neutral: '#fef3c7',
+          'base-100': '#ffffff',
+          info: '#60a5fa',
+          success: '#4ade80',
+          warning: '#fbbf24',
+          error: '#ff0000',
+        },
+        jspmDark: {
+          primary: '#fff',
+          secondary: '#ff0000',
+          accent: '#facc15',
+          neutral: '#111827',
+          'base-100': '#000',
+          info: '#60a5fa',
+          success: '#4ade80',
+          warning: '#fbbf24',
+          error: '#ff0000',
+        },
+      }
+    ],
+  }
 }
