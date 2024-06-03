ba455c659e7be7b49514107178b9770634a7b8eb
Jeff Wainwright <yowainwright@gmail.com>
2024-06-02 23:23:21 -0700

    chore: updates to make daisyui work

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,8 @@
-import cssnano from 'cssnano';
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default {
   plugins: [
-    cssnano,
     tailwindcss,
     autoprefixer
   ]
